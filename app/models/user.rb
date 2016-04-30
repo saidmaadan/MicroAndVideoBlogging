1f54a45ba18889f6794b9d8723ee3c00486fa245
Said Maadan <saidmaadan@users.noreply.github.com>
2016-04-30 15:07:45 -0500

    Added tiddle to create UserAuthenticationToken and acts_as_follow for users to follow each other

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,14 @@
 class User < ActiveRecord::Base
+  acts_as_followable
+  acts_as_follower
+  
+  validates_presence_of :username
+  validates_uniqueness_of :isername
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :token_authenticatable
+
+  has_many :authentication_tokens
 end
